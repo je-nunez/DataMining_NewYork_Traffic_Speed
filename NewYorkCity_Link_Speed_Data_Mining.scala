bf474639d201565e48be6cdd8ba0cbe68d643635
Jose E. Nunez <je.nunez@yahoo.ca>
2015-06-28 17:09:53 -0400

    parser in two-passes for New York Traffic Speed to WEKA ARFF format

--- NewYorkCity_Link_Speed_Data_Mining.scala
+++ NewYorkCity_Link_Speed_Data_Mining.scala
@@ -1,19 +1,25 @@
+#!/usr/bin/env scala
+
 import sys.process._
 import _root_.java.net.URL
 import _root_.java.io.File
-import _root_.java.util.regex._
 import _root_.java.text.SimpleDateFormat
+
+import weka.core.Instances
+import weka.core.converters.ArffSaver
+import weka.core.converters.CSVLoader
+
 
 
 val NYC_Traffic_Speed_URL = "http://real2.nyctmc.org/nyc-links-cams/LinkSpeedQuery.txt"
 
 
-def write_WEKA_attribute_header(header_line: String,
-                                arff_file: java.io.FileWriter) {
+def write_clean_CSV_header(header_line: String,
+                                clean_csv_file: java.io.FileWriter) {
 
       var attributes: Array[java.lang.String] = header_line.
                                                   replaceAllLiterally("\"", "").
-                                                  split("\\s+")
+                                                  split("\t")
 
       /* Check if the attributes in the NYC header line are our
        * expected attributes, so this parser is quite strict.
@@ -42,36 +48,15 @@ def write_WEKA_attribute_header(header_line: String,
 
       if( attributes.deep != expected_attributes.deep )
           // raise exception (and possibly abort, since the ETL parser needs
-          // to be changed
+          // to be changed)
           throw new Exception("Format of New York City attributes has changed.")
 
-      // Print the ARFF header to the file
-      arff_file.write("@relation New_York_City_Link_Speed\n\n")
-
-      // Print attributes to the ARFF file
-
-      arff_file.write("@attribute Id numeric\n")
-      arff_file.write("@attribute Speed numeric\n")
-      arff_file.write("@attribute TravelTime numeric\n")
-      arff_file.write("@attribute Status numeric\n")
-      arff_file.write("@attribute DataAsOf DATE \"M/d/yyy HH:mm:ss\"\n")
-      arff_file.write("@attribute linkId string\n")
-      arff_file.write("@attribute linkPoints string\n")
-      arff_file.write("@attribute EncodedPolyLine string\n")
-      arff_file.write("@attribute EncodedPolyLineLvls string\n")
-      arff_file.write("@attribute Owner string\n")
-      arff_file.write("@attribute Transcom_id string\n")
-      arff_file.write("@attribute Borough string\n")
-      arff_file.write("@attribute linkName string\n")
-
-      // Write an empty line after the ARFF attributes
-      arff_file.write("\n")
-
-      // The ARFF data begins now
-      arff_file.write("@data\n")
+      // The header line was verified: write it to the CSV file
+      clean_csv_file.write(header_line + "\n")
 }
 
-def write_WEKA_data_line(data_line: String, arff_file: java.io.FileWriter) {
+def write_clean_CSV_data_line(data_line: String,
+                              clean_csv_file: java.io.FileWriter) {
 
       // a better parser is needed of the lines that the
       // New York City Link Speed Query gives us
@@ -81,60 +66,34 @@ def write_WEKA_data_line(data_line: String, arff_file: java.io.FileWriter) {
                                                  stripSuffix("\"").
                                                  split("\"\\s+\"")
 
-      // We expect that the parsing above returned 13 fields in this array
+      // We expect that the parsing above returned 13 fields in this line
 
       if(line_values.length != 13) {
           System.err.println("WARNING: Ignoring line: " + data_line)
           return
       }
 
-      val weka_special_chars = Pattern.compile("[ ,]")
-
+      // Validate that the fifth field of the New York City Traffic Speed is
+      // a date in the format "M/d/yyyy HH:mm:ss"
       val date_fmt = "M/d/yyyy HH:mm:ss"
       // JODA is better
       val nyc_date_time_fmt = new java.text.SimpleDateFormat(date_fmt)
       nyc_date_time_fmt.setLenient(false)
 
-      var column_number = 1
-
-      for (value <- line_values) {
-           val s = value.stripPrefix("\"").stripSuffix("\"")
-
-           // Verify that we have a valid record,
-           // by column #5 being a date-time format
-           if (column_number == 5) {
-               try {
-                   val d = nyc_date_time_fmt.parse(s)
-               } catch {
-                   case e: java.text.ParseException => {
-                      System.err.println("WARNING: Ignoring line: " + data_line)
-                      return
-                   }
-              }
-           }
-
-           if (s.isEmpty)
-               arff_file.write(",")
-           else {
-               if (column_number > 1)
-                   arff_file.write(",")
-               arff_file.write("\"" + value + "\"")
-               // if (s.indexOf(' ') != -1)
-               // if (s.matches(weka_special_chars))
-               /*
-               if (weka_special_chars.matcher(s).find)
-                   arff_file.write("\"" + s + "\"")
-               else
-                   arff_file.write(s)
-               */
-           }
-           column_number += 1
+      try {
+          val d = nyc_date_time_fmt.parse(line_values(4))
+      } catch {
+          case e: java.text.ParseException => {
+               System.err.println("WARNING: Ignoring line: " + data_line)
+               return
+          }
       }
-      // Write the new-line, that is the end of the WEKA record
-      arff_file.write("\n")
+
+      // Write this data line to the CSV file
+      clean_csv_file.write(data_line + "\n")
 }
 
-def download_NYC_TrafficSpeed_to_WEKA_arff(src_url: String, dest_file: String) {
+def download_NYC_TrafficSpeed_to_clean_CSV(src_url: String, dest_file: String) {
       try {
             val src = scala.io.Source.fromURL(src_url)
             val out = new java.io.FileWriter(dest_file)
@@ -144,9 +103,9 @@ def download_NYC_TrafficSpeed_to_WEKA_arff(src_url: String, dest_file: String) {
             for (line <- src.getLines) {
                 // out.write(line + "\n")
                 if ( line_number == 1 )
-                     write_WEKA_attribute_header(line, out)
+                     write_clean_CSV_header(line, out)
                 else
-                     write_WEKA_data_line(line, out)
+                     write_clean_CSV_data_line(line, out)
                 line_number += 1
             }
 
@@ -159,7 +118,47 @@ def download_NYC_TrafficSpeed_to_WEKA_arff(src_url: String, dest_file: String) {
       }
 }
 
+def convert_clean_CSV_to_WEKA_ARFF(src_csv: String, dest_arff: String) {
 
-download_NYC_TrafficSpeed_to_WEKA_arff(NYC_Traffic_Speed_URL, "temp_file.arff")
+      // Modified from https://weka.wikispaces.com/Converting+CSV+to+ARFF
+      // to add the format of our dates, and nominal attributes
 
+      var cvs_in: CSVLoader = new CSVLoader();
+      /* Some options for the New York City's Traffic Speed CVS, ie., which
+       * attributes we know are date (and which date format), and which
+       * attributes we know are string (and not to be analyzed as nominal
+       * values)
+       */
+      cvs_in.setOptions(Array(
+                              "-D", "5",
+                              "-format", "M/d/yyyy HH:mm:ss",
+                              "-S", "7-9"
+                             )
+                       );
+      cvs_in.setSource(new File(src_csv));
+      var instances_in: Instances = cvs_in.getDataSet();
+
+      var arff_out: ArffSaver = new ArffSaver();
+      arff_out.setInstances(instances_in);
+      arff_out.setFile(new File(dest_arff));
+      arff_out.writeBatch();
+}
+
+
+// The temporary filename is "New_York_City_Link_Speed.csv" because WEKA will
+// use the filename (without extension) as the @relation name in the ARFF file
+
+val temp_csv_fname = "New_York_City_Link_Speed.csv"
+
+// First pass of the parser: download URL and clean the records to a CSV file
+
+download_NYC_TrafficSpeed_to_clean_CSV(NYC_Traffic_Speed_URL, temp_csv_fname)
+
+// Second pass of the parser: convert CSV to ARFF, finding nominal attributes,
+// etc.
+
+val dest_csv_fname = "New_York_City_Link_Speed.arff"
+convert_clean_CSV_to_WEKA_ARFF(temp_csv_fname, dest_csv_fname)
+
+new File(temp_csv_fname).delete()
 

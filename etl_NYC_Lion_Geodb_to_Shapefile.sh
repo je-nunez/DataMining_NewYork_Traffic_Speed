12f10b45d501a910259c0776d017f87bad631bd1
Jose E. Nunez <je.nunez@yahoo.ca>
2015-08-17 22:53:45 -0400

    script to get and convert NYC LION Street geodb into shapefile
    This script is taken from the sister, smaller project to this
    one:
    
        https://github.com/je-nunez/Querying_NYC_Single_Line_Street_Base
    
    In this sister project, this script is more explained and has
    more checks (in Python) that could be brought here too into
    this project, but it can overblow it, and besides, the NYC
    LION GeoDB changes only once quarterly, if needed, according
    to:
    
        http://www.nyc.gov/html/dcp/pdf/bytes/lion_metadata.pdf?v=15b
    
               Maintenance ->
                    Update Frequency -> quarterly
    
    so this download and conversion script doesn't need to change
    too often, so its master development can be left inside its
    sister project.

---
+++ etl_NYC_Lion_Geodb_to_Shapefile.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+# We could have done this ETL on the GeoDB send its results
+# PostGIS in PostgreSQL, using the option
+#
+#    ogr2ogr -f "PostgreSQL" ...
+#
+# in the ETL below. We simplify for now in obtaining a
+# 'Shapefile' result db.
+
+# This is the SQL select statement that we use to do the ETL
+# (Extract-Transform-Load) on the New York City Single Line
+# Street Base Map. The documentation of the fields of this
+# input database are here:
+#
+#    http://www.nyc.gov/html/dcp/pdf/bytes/lion_metadata.pdf?v=15b
+#
+# One of the task of this ETL on this NYC geodb is to select
+# properly this SQL select statement. E.g., as it is now, this
+# SQL below for this ETL generates over 100 MB of results.
+#
+# Note that the geographical coordinates (XFrom, YFrom, XTo, YTo,
+# ArcCenterX, ArcCenterY) are included in this SQL query, as well
+# as other non-geographical attributes, as the 'street' name.
+
+ETL_SQL_select_transform="select segmentid, segmenttyp, segcount, XFrom, YFrom, XTo, YTo, ArcCenterX, ArcCenterY, street from lion"
+#
+# This SQL ETL below, which includes the field 'streetwidth', happens to
+# fail because the resulting dBase IV file in the ouput 'Shapefile' has
+# an invalid value in one of the more than a quater records, so we can't
+# use it. (Of couse, the 'streetwidth' field would be very useful for the
+# Data Mining because the both the Speed and the Volume Counts of Traffic
+# depend on the 'streetwidth'.)
+#
+# SQL_select_transform="select segmentid, segmenttyp, segcount, XFrom, YFrom, XTo, YTo, ArcCenterX, ArcCenterY, streetwidth, street from lion"
+
+# Abort this script on errors
+set -e
+
+# Download the New York City LION Single Line Street Base Map before ETL. Its
+# documentation is here:
+#
+#    http://www.nyc.gov/html/dcp/html/bytes/dwnlion.shtml
+
+echo "Downloading the NYC input database to ./nyc_src_db/in.zip ..."
+if [[ ! -d nyc_src_db ]]; then
+    mkdir nyc_src_db
+fi
+
+wget -O nyc_src_db/in.zip \
+     --quiet \
+     'http://www.nyc.gov/html/dcp/download/bytes/nyc_lion15b.zip'
+
+echo "Unzipping the NYC input File GeoDataBase ..."
+cd nyc_src_db
+unzip -qq  in.zip   # -qq: ultra quiet mode
+cd ..
+
+echo "Preparing ETL destination dir with the shapefile: ./etl_dest_shp/ ..."
+
+if [[ ! -d etl_dest_shp ]]; then
+    mkdir etl_dest_shp
+fi
+cd etl_dest_shp
+if [[ -f nyc_data_exploration.shp ]]; then
+    rm nyc_data_exploration*
+fi
+
+echo "Running the ETL properly using the SQL: '$ETL_SQL_select_transform'"
+
+# This is the Source Coordinate System: according to page 4 of:
+#
+#    http://www.nyc.gov/html/dcp/pdf/bytes/lion_metadata.pdf?v=15b
+#
+# The New York City LION database is in "EPSG:2263" coordinates for
+# the geometrical projection
+# (This can also be found doing a `ogrinfo -al -so ./nyc_src_db/lion.gdb/`
+# which is more handy to give summary of fields in NYC LION, which then
+# the documentation URL above explains)
+
+SRC_COORD=EPSG:2263
+
+# The destination coordinate system is WSG84 (World Geodetic System 1984)
+
+DST_COORD=EPSG:4326
+
+# Note: The reason we need to project coordinates-systems because, for the
+# data mining on the traffic, we need to join different Open Data sources of
+# New York City, like the New York City's Department of Transportation
+# Real-Time Link Speed, which are in other coordinate systems:
+#
+#   https://data.cityofnewyork.us/Transportation/Real-Time-Traffic-Speed-Data/xsat-x5sa
+#
+
+# ogr2ogr belongs to the 'gdal' yum package (Fedora) and 'gdal-bin' (Debian)
+# Mac users:  brew install gdal
+
+ogr2ogr nyc_data_exploration.shp ../nyc_src_db/lion.gdb/ \
+        -s_srs "$SRC_COORD" -t_srs "$DST_COORD" \
+        -sql "$ETL_SQL_select_transform"
+
+# The '.shp' contains the geographical attributes
+echo "Verifying if the resulting GIS shapefile is valid..."
+# ogrinfo is in the same package in Fedora, Debian, and Mac OS/X as above
+ogrinfo  nyc_data_exploration.shp
+

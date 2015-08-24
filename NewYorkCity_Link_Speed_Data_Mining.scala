a831b567aae15e5139621c3487d646e46f80ad59
Jose E. Nunez <je.nunez@yahoo.ca>
2015-08-23 21:13:22 -0400

    all object-oriented classes and objects are in their respective packages, so we can use in Scala a main object to wrap the main program.

--- NewYorkCity_Link_Speed_Data_Mining.scala
+++ NewYorkCity_Link_Speed_Data_Mining.scala
@@ -1,48 +1,15 @@
-#!/usr/bin/env scala -language:postfixOps -deprecation
+#!/usr/bin/env scala -deprecation
 
-import scala.App
-import sys.process._
-import scala.util.control.Exception._
-import _root_.java.io.File
-import _root_.java.net.URL
-import _root_.java.net.URLEncoder
-import _root_.java.text.SimpleDateFormat
 import _root_.java.util.Calendar
-import _root_.java.util.Date
 import scala.collection.JavaConversions._
-import scala.collection.mutable.ListBuffer
-import scala.math
 
-/* Polygon */
-
-import org.geoscript.geometry.io._
-import org.geoscript.geometry._
-import org.geoscript.viewer._
-import com.vividsolutions._
-import org.geotools.feature.DefaultFeatureCollection
-
-/*
- * GeoScript Polygon classes, since the New York City Real-Time
- * Link Speed URL:
- *
- *    http://real2.nyctmc.org/nyc-links-cams/LinkSpeedQuery.txt
- *
- * has a column, "linkPoints", with values like:
- *
- *    "40.74047,-74.009251 40.74137,-74.00893 40.7431706,-74.008591 ..."
- *
- * which represent the speed of the traffic in that polygon in New York City.
- *
- * Since the URL represents the speeds of traffic in different polygons inside
- * New York City, and the classes above in GeoScript/JTS allow to find the
- * intersection of polygons, etc, these polygonal operations will allow us to
- * find the bottlenecks or optimum (faster) paths around different points.
- *
- * Note: this is not the Polygon of the WEKA UserClassifier, in
- *      weka/classifiers/trees/UserClassifier.java
- * but the spatial polygon of the section of speed in NYC.
+/* Use GeoTools's DefaultFeatureCollection for the intersection
+ * between New York City's Department of City Planning LION
+ * Single-line Street Segments Geographical DB and the NYC
+ * Real-Time Traffic Speed per polygonal zone in the New York
  */
 
+import org.geotools.feature.DefaultFeatureCollection
 
 /* Internal packages of this project. In this very first version, they
  * have this simple prefix, but will surely change. */
@@ -56,75 +23,66 @@ import src.main.scala.opendata._
 import src.main.scala.model.Current_Real_Time_Traffic_Speeds
 
 
-
 /*
  *   MAIN PROGRAM
+ *      relying all the functionality to the application packages above
  *
  */
 
+object mainNewYorkCityLinkSpeedDataMining {
 
-def main() {
+  def main(args: Array[String]) {
 
-     /* Take the time of the download of the Real-Time Speed URL, because
-      * this time is used to correlate it with the samples of traffic volume
-      * counts
-      *
-      *      NYC_Traffic_Volume_Count_URL (see definition above)
-      *
-      * because the latter has samples per hour of the day and per day (of
-      * the week), ie., a sample for hour 12PM-1PM on Mondays, let's say,
-      * according to the City of New York's Open Data.
-      * (see definition of NYC_Traffic_Volume_Count_URL above)
-      */
+    // Take the time of the download of the Real-Time Speed URL, because
+    // this time is used to correlate it with the samples of traffic volume
+    // counts
+    //
+    //      NYC_Traffic_Volume_Count_URL (see definition above)
+    //
+    // because the latter has samples per hour of the day and per day (of
+    // the week), ie., a sample for hour 12PM-1PM on Mondays, let's say,
+    // according to the City of New York's Open Data.
+    // (see definition of NYC_Traffic_Volume_Count_URL above)
 
-     val download_time = Calendar.getInstance().getTime()
+    val download_time = Calendar.getInstance().getTime()
 
-     /* Convert the New York City Real-Time Speed URL to WEKA */
+    // Convert the New York City Real-Time Speed URL to WEKA
 
-     val realt_speed_downldr =
-                new convert_LinkSpeed_CSV_URL_to_WEKA_Serialized_insts(
+    val realt_speed_downldr =
+      new convert_LinkSpeed_CSV_URL_to_WEKA_Serialized_insts(
                                download_time
-                           )
+        )
 
-     realt_speed_downldr.ETL_OpenData_CSV_URL_into_WEKA()
+    realt_speed_downldr.ETL_OpenData_CSV_URL_into_WEKA()
 
-     val speed_model = new Current_Real_Time_Traffic_Speeds(
-                                   Config.Final_LinkSpeed_WEKA_SerInsts_fname
-                              )
+    val speed_model = new Current_Real_Time_Traffic_Speeds(
+                           Config.Final_LinkSpeed_WEKA_SerInsts_fname
+                         )
 
-     val polygon_realt_speeds =
-                     speed_model.load_traffic_speed_per_polygon_in_the_city()
+    val polygon_realt_speeds =
+      speed_model.load_traffic_speed_per_polygon_in_the_city()
 
-     /* Process the Traffic Volume Counts per segment and per hour to
-      * unite it with the Real-Time Traffic Speed.
-      */
+    // Process the Traffic Volume Counts per segment and per hour to
+    // unite it with the Real-Time Traffic Speed.
 
-     val traffic_downldr =
-             new convert_Traffic_Volume_Cnt_CSV_URL_to_WEKA_Serialized_insts(
-                           download_time
-                        )
+    val traffic_downldr =
+      new convert_Traffic_Volume_Cnt_CSV_URL_to_WEKA_Serialized_insts(
+                         download_time
+                     )
 
-     traffic_downldr.ETL_OpenData_CSV_URL_into_WEKA()
+    traffic_downldr.ETL_OpenData_CSV_URL_into_WEKA()
 
-     /* Open the ESRI Shapefile with the New York City LION Single-Line Street
-      * GeoDB. This ESRI Shapefile was a download from an ETL in project:
-      *
-      * https://github.com/je-nunez/Querying_NYC_Single_Line_Street_Base
-      *
-      */
+    // Open the ESRI Shapefile with the New York City LION Single-Line Street
+    // GeoDB. This ESRI Shapefile was a download from an ETL in project:
+    //
+    // https://github.com/je-nunez/Querying_NYC_Single_Line_Street_Base
 
-     val nyc_lion = new ExistingShapefile(Config.NYC_LION_Polyline_Street_Map)
-     nyc_lion.open()
-     val lion_in_realt_speeds = new DefaultFeatureCollection()
-     nyc_lion.filter_geom_features(polygon_realt_speeds, lion_in_realt_speeds)
+    val nyc_lion = new ExistingShapefile(Config.NYC_LION_Polyline_Street_Map)
+    nyc_lion.open()
+    val lion_in_realt_speeds = new DefaultFeatureCollection()
+    nyc_lion.filter_geom_features(polygon_realt_speeds, lion_in_realt_speeds)
 
-     println("Number of filtered segments: " + lion_in_realt_speeds.size)
-}
-
-
-/*
- * Entry point
- */
-
-main
+    println("Number of filtered segments: " + lion_in_realt_speeds.size)
+  }  // end of method main(...)
+} // end of object mainNewYorkCityLinkSpeedDataMining
 
